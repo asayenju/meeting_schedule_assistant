bd8412631eac039e48c57f6546b5d478f3ada0d1
Ashwin Sayenju <ashwinsayenju@Ashwins-MacBook-Air.local>
2025-11-08 00:47:21 -0500

    Get google services and confirm to use the service

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,24 @@
+# main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api.auth import router as auth_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],     # allow all origins (safe for dev)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(auth_router, prefix="/api", tags=["Auth"])
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {
+        "message": "Welcome to Google API Demo",
+        "routes": ["/api/auth/login", "/api/calendar/freebusy", "/api/gmail/messages"]
+    }
